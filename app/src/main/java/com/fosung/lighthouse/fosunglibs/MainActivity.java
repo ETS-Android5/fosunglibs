6094df638b89308dcbce6d57c2e6af0326ef88ad
liangxiao6 <liangxiao6@live.com>
2022-03-30 23:24:10 +0800

    merger

--- app/src/main/java/com/fosung/lighthouse/fosunglibs/MainActivity.java
+++ app/src/main/java/com/fosung/lighthouse/fosunglibs/MainActivity.java
@@ -1,6 +1,5 @@
 package com.fosung.lighthouse.fosunglibs;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         new PgyerSDKManager.Init()
                 .setContext(getApplicationContext()) //设置上下问对象
                 .start();
-        startActivity(new Intent(MainActivity.this, ScanAct2.class));
+//        startActivity(new Intent(MainActivity.this, ScanAct2.class));
 
 
     }
